5b61c5ab7b5dd2600f0a4c45ee06516a743a7c2a
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-10 13:51:14 +0700

    menambahkan ef core

---
+++ SampleWebAPICore/Controllers/PasienController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using SampleWebAPICore.Models;
+
+namespace SampleWebAPICore.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Pasien")]
+    public class PasienController : Controller
+    {
+        static List<Pasien> lstPasien = new List<Pasien>
+        {
+            new Pasien{PasienID=1,Nama="Budi",Alamat="Jl Mangga 12",Telpon="667788",Umur=45},
+            new Pasien{PasienID=2,Nama="Amir",Alamat="Jl Duren 12",Telpon="889988",Umur=48},
+            new Pasien{PasienID=3,Nama="Bambang",Alamat="Jl Sawo 12",Telpon="445566",Umur=24}
+        };
+
+        [HttpGet]
+        public IEnumerable<Pasien> Get()
+        {
+            return lstPasien;
+        }
+
+        /*[HttpGet]
+        [Route("GetPasien")]
+        public IEnumerable<Pasien> GetPasien()
+        {
+            return lstPasien;
+        }*/
+    }
+}
---
+++ SampleWebAPICore/Data/PasienDataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SampleWebAPICore.Models;
+
+namespace SampleWebAPICore.Data
+{
+    public class PasienDataContext : DbContext
+    {
+        public PasienDataContext(DbContextOptions<PasienDataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Pasien> Pasien { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pasien>().ToTable("Pasien");
+        }
+
+    }
+}
