58317efadf19f7a65d52ef2de4c271a435b41dd5
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-10 12:42:07 +0700

    Add project files.

---
+++ SampleWebAPICore/Models/Pasien.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebAPICore.Models
+{
+    public class Pasien
+    {
+        public int PasienID { get; set; }
+        public string Nama { get; set; }
+        public string Alamat { get; set; }
+        public int Umur { get; set; }
+        public string Telpon { get; set; }
+    }
+}
