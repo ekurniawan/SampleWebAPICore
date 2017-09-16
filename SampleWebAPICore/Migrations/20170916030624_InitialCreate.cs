2c71dbf5724a787ff24cb5b6fa3b9b7941f0de34
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-16 10:08:48 +0700

    menambahkan primary key di pengguna

---
+++ SampleWebAPICore/Migrations/20170916030624_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SampleWebAPICore.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pasien",
+                columns: table => new
+                {
+                    PasienID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Alamat = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nama = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telpon = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Umur = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pasien", x => x.PasienID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pengguna",
+                columns: table => new
+                {
+                    Username = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pengguna", x => x.Username);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pasien");
+
+            migrationBuilder.DropTable(
+                name: "Pengguna");
+        }
+    }
+}
--- SampleWebAPICore/Models/Pengguna.cs
+++ SampleWebAPICore/Models/Pengguna.cs
@@ -9,6 +9,7 @@ namespace SampleWebAPICore.Models
 {
     public class Pengguna
     {
+        //menambahkan primary key
         [Key]
         public string Username { get; set; }
         public string Password { get; set; }
